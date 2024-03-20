f76fe208f441a6519a86020b1c2cede57a281881
Darshan <darshan@essentially.ai>
2024-03-20 19:17:52 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,239 @@
-import Image from "next/image";
+"use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "",
+  }),
+  email: z.string().min(2, {
+    message: "",
+  }),
+  phone: z.string().min(2, {
+    message: "",
+  }),
+})
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+
+  }
+  return (
+    <main className="flex min-h-screen flex-row justify-between">
+      <div className="w-[40%] border-r-2 mt-20 mb-32">
+        <img
+          src="/logo.jpg"
+          alt="Vercel Logo"
+          className="dark:invert w-[600px] h-[600px] pb-20"
         />
       </div>
+      <div className="w-[50%] pl-10 mt-24">
+        <Tabs defaultValue="register" className="w-[75%]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="register" className="text-lg">Register</TabsTrigger>
+            <TabsTrigger value="login" className="text-lg">Sign In</TabsTrigger>
+          </TabsList>
+          <TabsContent value="register" className="mt-10">
+            <Card>
+              <CardHeader>
+                <CardTitle>Register</CardTitle>
+                <CardDescription>
+                  New to 021 Trade? Register yourself
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <Form {...form}>
+                  <div className="w-full">
+                    <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
+                      <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-lg">Full Name</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Enter Your Name" className="h-12 text-md" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-lg">Email Id</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Enter Your Email" className="h-12 text-md" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-lg">Phone Number</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Enter Your Phone Number" className="h-12 text-md" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button variant="outline" className="bg-black text-white">Edit Profile</Button>
+                        </DialogTrigger>
+                        <DialogContent className="lg:max-w-[600px] bg-white">
+                          <div className="">
+                            <p className="text-2xl font-[600] text-center pt-16 pb-16">Please click on the link sent to your email.</p>
+                          </div>
+                        </DialogContent>
+                      </Dialog>
+                    </form>
+                  </div>
+                </Form>
+              </CardContent>
+            </Card>
+          </TabsContent>
+          <TabsContent value="login" className="mt-10">
+            <Card>
+              <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>
+                  Already Registered? Login to your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                {/* <Form {...form}>
+                  <div className="w-full">
+                    <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-6">
+                      <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-lg">Full Name</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Enter Your Name" className="h-12 text-md" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-lg">Email Id</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Enter Your Email" className="h-12 text-md" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="phone"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel className="text-lg">Phone Number</FormLabel>
+                            <FormControl>
+                              <Input placeholder="Enter Your Phone Number" className="h-12 text-md" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button variant="outline" className="bg-black text-white">submit</Button>
+                        </DialogTrigger>
+                        <DialogContent className="lg:max-w-[600px] bg-white">
+                          <div className="">
+                            <p className="text-2xl font-[600] text-center pt-16 pb-16">Please click on the link sent to your email.</p>
+                          </div>
+                        </DialogContent>
+                      </Dialog>
+                    </form>
+                  </div>
+                </Form> */}
+                <div className="flex w-full  items-center space-x-2 pt-10">
+                  <Input type="email" placeholder="Login with your Registered email" className="h-12" />
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button variant="outline" className="bg-black text-white h-12">submit</Button>
+                    </DialogTrigger>
+                    <DialogContent className="lg:max-w-[600px] bg-white">
+                      <div className="">
+                        <p className="text-2xl font-[600] text-center pt-16 pb-16">Please click on the link sent to your email.</p>
+                      </div>
+                    </DialogContent>
+                  </Dialog>
+                </div>
+                <div className="text-center pt-4 pb-4">
+                  <p className="text-xl">or</p>
+                </div>
+                <div className="px-6 sm:px-0 pb-48 h-12">
+                  <button type="button" className="text-white w-full h-12 bg-[#000] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center mr-2 mb-2"><svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>Login with Google<div></div></button>
+                </div>
+              </CardContent>
+            </Card>
+          </TabsContent>
+        </Tabs>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
 }
+
+
+
